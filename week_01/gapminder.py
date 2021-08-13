6b316181da2c5a08ae09ecab4c880c081ca8a49f
Felix Kosse <82140007+fkosse@users.noreply.github.com>
2021-08-13 08:55:48 +0200

    Delete week_01 directory

--- week_01/gapminder.py
+++
@@ -1,7 +0,0 @@
-import plotly_express as px
- 
-fig = px.scatter(px.data.gapminder(), x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country", size="pop", color="continent", hover_name="country", log_x = True, size_max=100, range_x=[100,100000], range_y=[25,90])
-
-fig.show()
-
-
