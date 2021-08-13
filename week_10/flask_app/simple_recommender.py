acf15b28cb5af524189e4a29175a258bf80ef5d2
fkosse <felixkosse@yahoo.de>
2021-08-13 09:55:06 +0200

    removed folders

--- week_10/flask_app/simple_recommender.py
+++
@@ -1,8 +0,0 @@
-from MovieRecommender import MovieRecommender
-
-
-def get_recommendations(base_dict):
-    movie_name = base_dict['name1']
-    u_rating = base_dict['rating1']
-    u = MovieRecommender(movie_name, u_rating)
-    return u.recommendations
